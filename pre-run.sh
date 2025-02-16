ebb3444d37141a9da055318c53d3fc6cf4743ba5
kvv1618 <varun.kesharaju@gmail.com>
2025-02-16 18:02:47 -0500

    add htop library installation

--- pre-run.sh
+++ pre-run.sh
@@ -4,7 +4,7 @@ set -e
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt-get -y install build-essential valgrind nano patch diffutils curl fakeroot git pkg-config gedit libssl-dev libncurses5-dev libelf-dev bison flex bc dkms rsync clang-format dwarves qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
-sudo apt install -y vim
+sudo apt install -y vim htop
 
 git config --global user.email varun.kesharaju@gmail.com
 git config --global user.name kvv1618
