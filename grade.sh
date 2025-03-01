f0e09ca9484097e55a7c82e31237cc8796687e82
Kesharaju Varun <varun.kesharaju@gmail.com>
2025-03-01 10:19:29 -0500

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -7,5 +7,6 @@ git config --global user.email varun.kesharaju@gmail.com
 git config --global user.name kvv1618
 
 git clone $1
+cd project0*
 cp ~/GraderOS/given_config.txt .config
 make -j$(($(nproc) - 2)) bindeb-pkg
