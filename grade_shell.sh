3f05b2391fd2597ffb1bdfad89dd6fc684edac1b
Kesharaju Varun <varun.kesharaju@gmail.com>
2025-03-13 18:39:24 -0400

    Create grade_shell.sh

---
+++ grade_shell.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+cd ~/Documents
+sudo rm -rf ~/Documents/*
+
+git config --global user.email varun.kesharaju@gmail.com
+git config --global user.name kvv1618
+
+git clone $1
+cd project1*
+make clean
+make
+
+exec_file=$(find . -maxdepth 1 -type f -executable | head -n 1)
+$exec_file
