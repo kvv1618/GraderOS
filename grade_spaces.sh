07abc5caf7a90b630225eb2e8964f9bde7273736
Kesharaju Varun <varun.kesharaju@gmail.com>
2025-04-18 12:31:39 -0400

    Update grade_spaces.sh

--- grade_spaces.sh
+++ grade_spaces.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 set -e
 
-cd ~/Documents
-sudo rm -rf ~/Documents/*
-
-git config --global user.email varun.kesharaju@gmail.com
-git config --global user.name kvv1618
-
-repo_url="https://github.com/UMBC-CMSC421/project2-$1.git"
-git clone "$repo_url"
-cd project2*
-
-cd user_space
-make clean
-make
-exec=$(find . -maxdepth 1 -type f -executable | head -n 1)
-valgrind --leak-check=full $exec
-
-cp ../tests/user_tests.c .
-sed -i '/^SRC/ s/\bmain\.c\b/user_tests.c/' Makefile
-make clean
-make
-exec=$(find . -maxdepth 1 -type f -executable | head -n 1)
-valgrind --leak-check=full $exec
-
 cd ../
 make clean
 cp ~/GraderOS/given_config.txt .config
@@ -31,8 +8,4 @@ make -j$(($(nproc) - 2)) bindeb-pkg
 
 cd user_space
 cp ../tests/kernel_tests.c .
-sed -i '/^SRC/ s/\buser_tests\.c\b/kernel_tests.c/' Makefile
-make clean
-make
-exec=$(find . -maxdepth 1 -type f -executable | head -n 1)
-valgrind --leak-check=full $exec
+./grade_kernel_space_test.sh
