0a81b68915510305561f61a33fbb7c1b9b8e25bc
Kesharaju Varun <varun.kesharaju@gmail.com>
2025-04-23 13:36:50 -0400

    Update grade_user_space_test.sh

--- grade_user_space_test.sh
+++ grade_user_space_test.sh
@@ -6,5 +6,5 @@ cd user_space
 cp ~/GraderOS/user_tests.Makefile Makefile
 make clean
 make
-exec=$(find . -maxdepth 1 -type f -executable | head -n 1)
+exec=$(find . -maxdepth 1 -type f -executable -printf '%T@ %p\n' | sort -nr | head -n 1 | cut -d' ' -f2-)
 valgrind --leak-check=full $exec
