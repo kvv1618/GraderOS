a270a1424c8d77744265ddbba25d689a81566495
Kesharaju Varun <varun.kesharaju@gmail.com>
2025-04-23 13:44:01 -0400

    Update grade_kernel_space_test.sh

--- grade_kernel_space_test.sh
+++ grade_kernel_space_test.sh
@@ -4,5 +4,5 @@ set -e
 cp ~/GraderOS/kernel_tests.Makefile Makefile
 make clean
 make
-exec=$(find . -maxdepth 1 -type f -executable | head -n 1)
+exec=$(find . -maxdepth 1 -type f -executable -printf '%T@ %p\n' | sort -nr | head -n 1 | cut -d' ' -f2-)
 valgrind --leak-check=full $exec
